a723d0fbe10cc7e644baf593385c75f474c00dab
Jan Pakos <janpakos123@gmail.com>
2025-05-25 13:18:03 +0200

    update

--- src/i18n.ts
+++ src/i18n.ts
@@ -4,17 +4,17 @@ import { initReactI18next } from 'react-i18next';
 
 // Import your translation files
 import enTranslations from './translations/en.json';
-import svTranslations from './translations/se.json';
+import seTranslations from './translations/se.json';
 import plTranslations from './translations/pl.json';
 
 const resources = {
   en: {
     translation: enTranslations
   },
-  sv: {
-    translation: svTranslations
+  se: {
+    translation: seTranslations
   },
-  es: {
+  pl: {
     translation: plTranslations
   }
 };
